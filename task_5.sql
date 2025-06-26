2416ee20d6f81ebeef0578eb6a224040b46148c4
kriscodesign <kriscodesign@gmail.com>
2025-06-26 16:32:16 +0100

    4th task

---
+++ task_5.sql
@@ -0,0 +1,12 @@
+USE alx_book_store;
+
+INSERT INTO Customer(
+    customer_id, 
+    customer_name,
+    email,
+    address ) VALUE (
+        1,
+        'Cole Baidoo',
+        'cbaidoo@sandtech.com',
+        '123 Happiness Ave'
+    );
