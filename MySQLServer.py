8ff2d7ed2d6142bb964857085a4b9377c21385f3
Kris <45769729+KrisCoDesign@users.noreply.github.com>
2025-06-26 10:58:40 +0100

    Add files via upload

---
+++ MySQLServer.py
@@ -0,0 +1,19 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="flash"
+    )
+
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created sucessfully!")
+
+except Error as e:
+    print(e)
+
+mycursor.close()
+mydb.close()
